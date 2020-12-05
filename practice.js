a605f4c88240fcdfa733c6bbfad15575116b173c
masterrychu <seth_jacob@icloud.com>
2020-12-05 10:01:14 -0700

    initial commit

---
+++ practice.js
@@ -0,0 +1,19 @@
+/* let userName = prompt("Who's there?", '');
+            
+            if (userName === 'Admin') {
+                let passWord = prompt('Password?', '');
+
+                if (passWord === 'TheMaster') {
+                    alert('Welcome!');
+                } else if (passWord === '' || passWord === null) {
+                    alert('Cancelled!');
+                } else {
+                    alert("Wrong password!")
+                }
+
+            } else if (userName === '' || userName === null) {
+                alert('Cancelled!');
+
+            } else {
+                alert("I don't know you!");
+            } */
